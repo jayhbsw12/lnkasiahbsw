699d7ff48d62a6eaeb867306d69054fb54f6249b
anjanahbsoftweb <jay.m@hbsoftweb.in>
2025-09-30 17:25:46 +0530

    updated

---
+++ partners.php
@@ -0,0 +1,219 @@
+<?php include("header.php"); ?>
+
+<body>
+    <section class="hero-banner">
+        <div class="hero-contact">
+            <div class="square-box"><img src="assets/images/square.svg">
+                <h4>Partners</h4>
+            </div>
+            <h1>Let's Do <span class="heading-gradient"> Products and Services? </span></h1>
+            <h3>Contact Us as a Trusted Partner</h3>
+        </div>
+        <img src="assets/images/hero-bottom.svg" class="hero-bottom-shape">
+    </section>
+
+    <section class="partner-section">
+        <div class="partner-container">
+            <div class="partner-column-left">
+                <div>
+                    <h2 class="heading-gradient">Vigyanlabs</h2>
+                    <span>IT Advisors for National ID Aadhar 2.0, India Post.</span>
+                </div>
+                <div class="partner-column-subsection">
+                    <span class="subsection-title">
+                        AI powered Sustainable solutions for
+                    </span>
+                    <div class="subsection-cards">
+                        <div class="subsection-card">
+                            <img src="./assets/images/partners/energy-icon.svg" alt="">
+                            <span>Energy Savings</span>
+                        </div>
+                        <div class="subsection-card">
+                            <img src="./assets/images/partners/endpoint-icon.svg" alt="">
+                            <span>Endpoint Management
+                            </span>
+                        </div>
+                        <div class="subsection-card">
+                            <img src="./assets/images/partners/industry-icon.svg" alt="">
+
+                            <span>Industry 4.0 Monitoring</span>
+                        </div>
+                        <div class="subsection-card">
+                            <img src="./assets/images/partners/cloud-icon.svg" alt="">
+
+                            <span>Green Virtual Private Cloud (VPC)</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="partner-column-subsection">
+                    <span class="subsection-title">
+                        7 US Patents , 6 million devices running on IPM+
+                    </span>
+                    <div class="subsection-list">
+                        <div class="subsection-list-item">
+                            <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                            <span>Enhanced Hardware Health (Less heat generation)</span>
+                        </div>
+
+                        <div class="subsection-list-item">
+                            <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                            <span>Improves productivity by extending Battery backup time and Battery Life</span>
+                        </div>
+
+                        <div class="subsection-list-item">
+                            <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                            <span>Patented AI Power Mind Technology saves up to 50% Energy Savings
+                                non-intrusively.</span>
+                        </div>
+
+                        <div class="subsection-list-item">
+                            <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                            <span>Everything that your Endpoints need on a single agent.</span>
+                        </div>
+                    </div>
+                </div>
+                <strong style="margin-top:20px">An ISO200001:2018,27001:2013,27017:2015,27018:2019</strong>
+                <strong>Certified Cloud Services in JV with NSDL Protean Cloud.</strong>
+            </div>
+            <div class="partner-column-right">
+                <input class="partner-column-image" type="image"
+                    src="https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?q=80&w=1176&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="" width="540px">
+                <button class="cta-btn">
+                    <span class="cta-text">More about us</span>
+                </button>
+            </div>
+        </div>
+    </section>
+
+    <section class="partner-section">
+        <div class="partner-container-2">
+            <div class="partner-column-right">
+                <input class="partner-column-image" type="image"
+                    src="https://images.unsplash.com/flagged/photo-1553267252-d100936057c1?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="" width="550px">
+                <button class="cta-btn">
+                    <span class="cta-text">More about us</span>
+                </button>
+            </div>
+            <div class="partner-column-left">
+                <div>
+                    <h2 class="heading-gradient">Alendei</h2>
+                    <h2 style="font-weight:400">A helping hand to growing business</h2>
+                </div>
+                <div>
+                    <span style="font-weight:500"><strong class="heading-gradient">What's App global CPaaS
+                            Platform:</strong> Reach your customer through verity of communication channel such as SMS,
+                        Chat bot.</span>
+                </div>
+                <div class="subsection-list" style="display: flex;gap: 10px;flex-direction: column;
+">
+                    <div class="subsection-list-item">
+                        <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                        <span>Enhanced Hardware Health (Less heat generation)</span>
+                    </div>
+
+                    <div class="subsection-list-item">
+                        <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                        <span>Improves productivity by extending Battery backup time and Battery Life</span>
+                    </div>
+
+                    <div class="subsection-list-item">
+                        <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                        <span>Patented AI Power Mind Technology saves up to 50% Energy Savings
+                            non-intrusively.</span>
+                    </div>
+
+                    <div class="subsection-list-item">
+                        <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                        <span>Everything that your Endpoints need on a single agent.</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="partner-section">
+        <div class="partner-container-3">
+            <div class="partner-column-left">
+                <div>
+                    <h2 class="heading-gradient">Simplify & Secure Your Network with Smart DDI Technology</h2>
+                    <strong>ISolutions that enables Network Automation and strengthens DNS Security with DDI Technology
+                        to control and secure anywhere access to your enterprise and cloud applications.</strong>
+                </div>
+
+                <div class="partner-column-subsection">
+                    <div class="subsection-list">
+                        <div class="subsection-list-item">
+                            <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                            <span>All-in-one technology encompassing DDI, DNS security, and application traffic
+                                management reduces your number of servers and lowers your TCO.</span>
+                        </div>
+
+                        <div class="subsection-list-item">
+                            <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                            <span>A unified and consistent GUI lets you see and search across all products, taking away
+                                the complexity of managing your network.</span>
+                        </div>
+
+                        <div class="subsection-list-item">
+                            <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                            <span>From a single platform, you can manage millions of IPs and over a thousand DNS and
+                                DHCP servers, to scale your business and maximize your ROI.</span>
+                        </div>
+
+                        <div class="subsection-list-item">
+                            <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                            <span>Protect better your data, users, and service continuity with our unique DNS security
+                                solution. It combines threat intelligence and real-time behavioral attack
+                                detection.</span>
+                        </div>
+
+                        <div class="subsection-list-item">
+                            <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                            <span>Protect better your data, users, and service continuity with our unique DNS security
+                                solution. It combines threat intelligence and real-time behavioral attack
+                                detection.</span>
+                        </div>
+
+                        <div class="subsection-list-item">
+                            <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                            <span>Protect better your data, users, and service continuity with our unique DNS security
+                                solution. It combines threat intelligence and real-time behavioral attack
+                                detection.</span>
+                        </div>
+                    </div>
+                </div>
+                <strong style="margin-top:20px">DDI & SOC Solutions is trusted by customers around the world in all
+                    industries</strong>
+            </div>
+            <div class="partner-column-right">
+                <input class="partner-column-image" type="image" src="assets\images\partners\demoimage-1.png" alt=""
+                    width="550px">
+                <button class="cta-btn">
+                    <span class="cta-text">More about us</span>
+                </button>
+            </div>
+        </div>
+    </section>
+
+    <section class="partner-section">
+        <h2 class="heading-gradient">Our Partners</h2>
+        <div class="partner-holder-container">
+            <div class="partner-holder">
+                <img src="assets\images\partners\odoo.png" alt="">
+            </div>
+            <div class="partner-holder">
+                <img src="assets\images\partners\microsoft.png" alt="">
+            </div>
+            <div class="partner-holder">
+                <img src="assets\images\partners\alendei.png" alt="">
+            </div>
+            <div class="partner-holder">
+                <img src="assets\images\partners\Vigyanlabs.png" alt="">
+            </div>
+        </div>
+    </section>
+</body>
+<?php include("footer.php"); ?>
