02d87b1797e4855999ae727af8dd63585fc766b9
anjanahbsoftweb <jay.m@hbsoftweb.in>
2025-09-30 19:22:06 +0530

    updated pricing

--- pricing.php
+++ pricing.php
@@ -3,7 +3,8 @@
 <body>
     <section class="hero-banner">
         <div class="hero-contact">
-            <div class="square-box"><img src="assets/images/square.svg">
+            <div class="square-box">
+                <img src="assets/images/square.svg">
                 <h4>Pricing</h4>
             </div>
             <h1>Pricing Plans to<span class="heading-gradient"> Fit your Team</span> <br>
@@ -15,8 +16,164 @@
 
     <section class="pricing-section">
         <div class="pricing-container">
-            <div class="pricing-left-container"></div>
-            <div class="pricing-right-container"></div>
+            <div class="pricing-left-container">
+                <span class="square-box">
+                    <img src="assets/images/square.svg">
+                    <span>ERP - ODOO Enterprise</span>
+                </span>
+
+                <h2>You are not <span class="heading-gradient">Dreaming!</span></h2>
+                <button class="cta-btn">
+                    <span class="cta-text">Contact Us</span>
+                </button>
+            </div>
+
+            <div class="pricing-right-container">
+                <div class="pricing-column-subsection">
+                    <span class="subsection-title">
+                        What's included in these plans:
+                    </span>
+                    <div class="subsection-list">
+                        <div class="subsection-list-item">
+                            <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                            <span>Enterprise plans include access to hundreds of Odoo apps on scalable cloud
+                                infrastructure, including hosting, incremental daily backups on two continents, email
+                                integration, top-notch security, 24/7 monitoring, and a control center to manage your
+                                Odoo environment.</span>
+                        </div>
+
+                        <div class="subsection-list-item">
+                            <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                            <span>Our service includes unlimited access to our support by email, from Monday to Friday,
+                                24/5, in English.</span>
+                        </div>
+
+                        <div class="subsection-list-item">
+                            <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                            <span>Access to our eLearning platform is free too. Upgrades are also included in all our
+                                plans at no extra fee.</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="pricing-column-subsection">
+                    <span class="subsection-title">
+                        What's not included in these plans:
+                    </span>
+                    <div class="subsection-list">
+                        <div class="subsection-list-item">
+                            <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                            <span>Hosting on Odoo.sh for custom developments (hosting on Odoo Online is free)</span>
+                        </div>
+
+                        <div class="subsection-list-item">
+                            <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                            <span>Implementation service and access to Odoo expert services</span>
+                        </div>
+
+                        <div class="subsection-list-item">
+                            <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                            <span>In-App-Purchase credits: SMS, contacts auto-complete, lead generation, IA to scan
+                                vendor bills & expenses</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+
+    <section class="pricing-section">
+        <div class="pricing-cards-container">
+            <div class="pricing-card">
+                <h3 style="color:#352F75">One App Free</h3>
+                <span>Free Forever</span>
+
+                <hr>
+                <div style="display: flex; justify-content: start;">
+                    <strong>Rs</strong>
+                    <span style="font-size:40px; font-weight: 500; color: #352F75;">0</span>
+                </div>
+                <hr>
+                <div>For <strong>One App Only</strong>. Unlimited users Odoo Online</div>
+            </div>
+            <div class="pricing-card">
+                <h3 style="color:#352F75">One App Free</h3>
+                <span>User/Month</span>
+                <hr>
+                <div style="display: flex; justify-content: start;">
+                    <strong>Rs</strong>
+                    <span style="font-size:40px; font-weight: 500; color: #352F75;">580</span><strong
+                        style="font-size:20px; text-decoration: line-through; text-decoration-color: red;">/725
+                        Rs*</strong>
+                </div>
+                <hr>
+                <strong>All Apps</strong>
+                <div class="subsection-list">
+                    <div class="subsection-list-item">
+                        <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                        <span>Odoo Online</span>
+                    </div>
+                </div>
+            </div>
+            <div class="pricing-card">
+                <h3 style="color:#352F75">One App Free</h3>
+                <span>User/Month</span>
+                <hr>
+                <div style="display: flex; justify-content: start;">
+                    <strong>Rs</strong>
+                    <span style="font-size:40px; font-weight: 500; color: #352F75;">890</span><strong
+                        style="font-size:20px; text-decoration: line-through; text-decoration-color: red;">/1150
+                        Rs*</strong>
+                </div>
+                <hr>
+                <strong>All Apps</strong>
+                <div class="subsection-list">
+                    <div class="subsection-list-item">
+                        <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                        <span>Odoo Online / Odoo.sh ‘ ‘ / On-Premise</span>
+                    </div>
+                    <div class="subsection-list-item">
+                        <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                        <span>Odoo Studio</span>
+                    </div>
+                    <div class="subsection-list-item">
+                        <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                        <span>Multi-Company</span>
+                    </div>
+                    <div class="subsection-list-item">
+                        <input type="image" src="./assets/images/partners/checklist-icon.svg" alt="">
+                        <span>External API</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div style="text-align: center">The Standard and Custom plans include all apps for a single fee: <br> With no
+            hidden costs, no limit on features or data: enjoy real transparency! <br> (*) The discount is valid for 12
+            months, for initial users ordered.(**) Cost for Odoo.sh hosting not
+            included.</div>
+    </section>
+
+    <section class="pricing-section">
+        <div class="pricing-container" style="gap:60px">
+            <div class="pricing-left-container">
+                <img src="assets\images\pricing\customization.png" alt="" width="">
+            </div>
+            <div class="pricing-right-container">
+                <span class="square-box" style="display: flex; gap: 10px;">
+                    <img src="assets/images/square.svg">
+                    <span>ERP - ODOO Enterprise</span>
+                </span>
+                <h2> <span class="heading-gradient">Customization</span></h2>
+                <p>
+                    We offer personalized Odoo consultations that carve a path toward your success. we are Odoo Ready
+                    partner. <br>
+                    We reshape Odoo to match your unique business needs, providing customization that mirrors your
+                    business identity and profitability. <br>
+                    If you need customizations, implementation service, project management, business needs analysis,
+                    data import, developments, training, etc.
+                </p>
+                <button class="cta-btn">
+                    <span class="cta-text">Contact Us</span>
+                </button>
+            </div>
         </div>
     </section>
 </body>
